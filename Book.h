81614d90c22913fbe722b6d3c08f073336b59c86
Mattichs <mattiabastia@gmail.com>
2022-11-16 12:05:03 +0100

    classe

---
+++ Book.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+class Book {
+    string ISBN;
+    string titolo;
+    string nome;
+    string cognome;
+    Date copiright;
+    bool stato;
+    
+    public:
+}
