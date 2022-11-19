f8c27eab3ed9b20cf78cf15539ede068bf0c783a
Mattichs <mattiabastia@gmail.com>
2022-11-19 10:32:13 +0100

    Inserito il controllo ISBN

--- Book.h
+++ Book.h
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
+#include "control.h"
 
 class Book {
-    string ISBN;
-    string titolo;
-    string nome;
-    string cognome;
-    Date copiright;
+    std::string ISBN; // tipo "111-222-333-444-X" X puo' essere lettera o numero
+    std::string titolo;
+    std::string nome;
+    std::string cognome;
+    //Date copiright;
     bool stato;
     
     public:
-}
+    /* 
+    Book(string s) {
+        if( controllo(s)) ISBN = s;
+        else throw exception
+    } */
+};
--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+#include <iostream>
+#include "Book.h"
+
 int main() {
+    if(control("111-222-333-X")) {
+        cout << "ISBN valido";
+    } else {
+        cout << "ISBN non valido (coglione)";
+    }
+    
     return 0;
 }
